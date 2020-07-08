1453f73f8cce704d289363be23acbdf3b1b2fbb8
vcloarec <vcloarec@gmail.com>
2020-07-07 21:03:23 -0400

    first step

--- src/gui/qgsgeometryrubberband.cpp
+++ src/gui/qgsgeometryrubberband.cpp
@@ -30,7 +30,6 @@ QgsGeometryRubberBand::QgsGeometryRubberBand( QgsMapCanvas *mapCanvas, QgsWkbTyp
 
 QgsGeometryRubberBand::~QgsGeometryRubberBand()
 {
-  delete mGeometry;
 }
 
 void QgsGeometryRubberBand::paint( QPainter *painter )
@@ -59,6 +58,9 @@ void QgsGeometryRubberBand::paint( QPainter *painter )
   paintGeom->transform( mMapCanvas->getCoordinateTransform()->transform() );
   paintGeom->draw( *painter );
 
+  if ( !mDrawVertices )
+    return;
+
   //draw vertices
   QgsVertexId vertexId;
   QgsPoint vertex;
@@ -66,6 +68,11 @@ void QgsGeometryRubberBand::paint( QPainter *painter )
   {
     drawVertex( painter, vertex.x(), vertex.y() );
   }
+}
+
+QgsWkbTypes::GeometryType QgsGeometryRubberBand::geometryType() const
+{
+  return mGeometryType;
 }
 
 void QgsGeometryRubberBand::drawVertex( QPainter *p, double x, double y )
@@ -106,8 +113,7 @@ void QgsGeometryRubberBand::drawVertex( QPainter *p, double x, double y )
 
 void QgsGeometryRubberBand::setGeometry( QgsAbstractGeometry *geom )
 {
-  delete mGeometry;
-  mGeometry = geom;
+  mGeometry.reset( geom );
 
   if ( mGeometry )
   {
@@ -149,10 +155,181 @@ void QgsGeometryRubberBand::setBrushStyle( Qt::BrushStyle brushStyle )
   mBrush.setStyle( brushStyle );
 }
 
+void QgsGeometryRubberBand::setIsVerticesDrawn( bool isVerticesDrawn )
+{
+  mDrawVertices = isVerticesDrawn;
+}
+
 QgsRectangle QgsGeometryRubberBand::rubberBandRectangle() const
 {
   qreal scale = mMapCanvas->mapUnitsPerPixel();
   qreal s = ( mIconSize - 1 ) / 2.0 * scale;
   qreal p = mPen.width() * scale;
   return mGeometry->boundingBox().buffered( s + p );
+}
+
+QgsCurveRubberBand::QgsCurveRubberBand( QgsMapCanvas *mapCanvas, QgsWkbTypes::GeometryType geomType ):
+  QgsGeometryRubberBand( mapCanvas, geomType )
+{}
+
+QgsCurve *QgsCurveRubberBand::curve()
+{
+  if ( mPoints.empty() )
+    return nullptr;
+
+  switch ( mStringType )
+  {
+    case QgsWkbTypes::LineString:
+      return new QgsLineString( mPoints ) ;
+      break;
+    case QgsWkbTypes::CircularString:
+      if ( mPoints.count() != 3 )
+        return nullptr;
+      return new QgsCircularString(
+               mPoints[0],
+               mPoints[1],
+               mPoints[2] ) ;
+      break;
+    default:
+      return nullptr;
+  }
+}
+
+bool QgsCurveRubberBand::curveIsComplete() const
+{
+  return ( mStringType == QgsWkbTypes::LineString && mPoints.count() > 1 ) ||
+         ( mStringType == QgsWkbTypes::CircularString && mPoints.count() > 2 );
+}
+
+void QgsCurveRubberBand::reset( QgsWkbTypes::GeometryType geomType )
+{
+  if ( !( geomType == QgsWkbTypes::LineGeometry || geomType == QgsWkbTypes::PolygonGeometry ) )
+    return;
+
+  mPoints.clear();
+  QgsGeometryRubberBand::reset( geomType );
+}
+
+void QgsCurveRubberBand::addPoint( const QgsPointXY &point, bool doUpdate )
+{
+  if ( mPoints.count() == 0 )
+    mPoints.append( QgsPoint( point ) );
+
+  mPoints.append( QgsPoint( point ) );
+
+  if ( doUpdate )
+    updateCurve();
+}
+
+void QgsCurveRubberBand::movePoint( const QgsPointXY &point )
+{
+  if ( mPoints.count() > 0 )
+    mPoints.last() = QgsPoint( point );
+
+  updateCurve();
+}
+
+void QgsCurveRubberBand::movePoint( int index, const QgsPointXY &point )
+{
+  if ( mPoints.count() > 0 && mPoints.size() > index )
+    mPoints[index] = QgsPoint( point );
+
+  updateCurve();
+}
+
+int QgsCurveRubberBand::pointsCount()
+{
+  return mPoints.size();
+}
+
+void QgsCurveRubberBand::setFirstPolygonPoint( const QgsPointXY &point )
+{
+  mFirstPolygonPoint = QgsPoint( point );
+}
+
+QgsWkbTypes::Type QgsCurveRubberBand::stringType() const
+{
+  return mStringType;
+}
+
+void QgsCurveRubberBand::setStringType( const QgsWkbTypes::Type &type )
+{
+  if ( ( type != QgsWkbTypes::CircularString && type != QgsWkbTypes::LineString ) || type == mStringType )
+    return;
+
+  mStringType = type;
+  if ( type == QgsWkbTypes::LineString && mPoints.count() == 3 )
+  {
+    mPoints.removeAt( 1 );
+  }
+
+  updateCurve();
+}
+
+void QgsCurveRubberBand::setGeometry( QgsAbstractGeometry *geom )
+{
+  QgsGeometryRubberBand::setGeometry( geom );
+}
+
+void QgsCurveRubberBand::updateCurve()
+{
+  std::unique_ptr<QgsCurve> curve;
+  switch ( mStringType )
+  {
+    case  QgsWkbTypes::LineString:
+      curve.reset( createLinearString() );
+      break;
+    case  QgsWkbTypes::CircularString:
+      curve.reset( createCircularString() );
+      break;
+    default:
+      return;
+      break;
+  }
+
+  if ( geometryType() == QgsWkbTypes::PolygonGeometry )
+  {
+    std::unique_ptr<QgsCurvePolygon> geom( new QgsCurvePolygon );
+    geom->setExteriorRing( curve.release() );
+    setGeometry( geom.release() );
+    return;
+  }
+
+  if ( geometryType() == QgsWkbTypes::LineGeometry )
+  {
+    setGeometry( curve.release() );
+  }
+}
+
+QgsCurve *QgsCurveRubberBand::createLinearString()
+{
+  std::unique_ptr<QgsLineString> curve( new QgsLineString );
+  if ( geometryType() == QgsWkbTypes::PolygonGeometry )
+  {
+    QgsPointSequence points = mPoints;
+    points.prepend( mFirstPolygonPoint );
+    curve->setPoints( points );
+  }
+  else
+    curve->setPoints( mPoints );
+
+  return curve.release();
+}
+
+QgsCurve *QgsCurveRubberBand::createCircularString()
+{
+  std::unique_ptr<QgsCircularString> curve( new QgsCircularString );
+  curve->setPoints( mPoints );
+  if ( geometryType() == QgsWkbTypes::PolygonGeometry )
+  {
+    // add a linear string to close the polygon
+    std::unique_ptr<QgsCompoundCurve> polygonCurve( new QgsCompoundCurve );
+    polygonCurve->addVertex( mFirstPolygonPoint );
+    if ( !mPoints.empty() )
+      polygonCurve->addVertex( mPoints.first() );
+    polygonCurve->addCurve( curve.release() );
+    return polygonCurve.release();
+  }
+  else
+    return curve.release();
 }
