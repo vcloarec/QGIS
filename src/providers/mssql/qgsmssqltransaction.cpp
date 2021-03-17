c456f473220e3ef8752c175d76e1501d6f15b202
vcloarec <vcloarec@gmail.com>
2021-03-17 08:56:19 -0400

    thread safe mssql connection

---
+++ src/providers/mssql/qgsmssqltransaction.cpp
@@ -0,0 +1,368 @@
+/***************************************************************************
+  qgsmssqltransaction.cpp - QgsMssqlTransaction
+
+ ---------------------
+ begin                : 11.3.2021
+ copyright            : (C) 2021 by Vincent Cloarec
+ email                : vcloarec at gmail dot com
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+#include <QSqlQuery>
+#include <QSqlDriver>
+#include <QSqlError>
+#include <QDebug>
+#include <QUuid>
+
+#include "qgsmssqltransaction.h"
+#include "qgsmssqlconnection.h"
+#include "qgsdatasourceuri.h"
+#include "qgsmessagelog.h"
+
+
+QgsMssqlTransaction::QgsMssqlTransaction( const QString &connString ): QgsTransaction( connString )
+{
+  QgsDataSourceUri uri( connString );
+
+  mDataBase = QgsMssqlConnection::getDatabaseConnection( uri, connString + QStringLiteral( "-transaction" ) );
+}
+
+QgsMssqlTransaction::~QgsMssqlTransaction()
+{
+  if ( QSqlDatabase::contains( mDataBase.connectionName() ) )
+    QSqlDatabase::removeDatabase( mDataBase.connectionName() );
+}
+
+bool QgsMssqlTransaction::executeSql( const QString &sql, QString &error, bool isDirty, const QString &name )
+{
+  if ( !mDataBase.isValid() )
+    return false;
+
+  if ( !mDataBase.isOpen() )
+    mDataBase.open();
+
+  if ( !mDataBase.isOpen() )
+    return false;
+
+  if ( isDirty )
+  {
+    QgsTransaction::createSavepoint( error );
+    if ( ! error.isEmpty() )
+    {
+      return false;
+    }
+  }
+
+  QSqlQuery query( mDataBase );
+  if ( !query.exec( sql ) )
+  {
+    if ( isDirty )
+      rollbackToSavepoint( savePoints().last(), error );
+
+    QString msg = tr( "MSSQL query failed: %1" ).arg( query.lastError().text() );
+    qDebug() << msg;
+    if ( error.isEmpty() )
+      error = msg;
+    else
+      error = QStringLiteral( "%1\n%2" ).arg( error, msg );
+
+    return false;
+  }
+
+  if ( isDirty )
+  {
+    dirtyLastSavePoint();
+    emit dirtied( sql, name );
+  }
+
+  return true;
+}
+
+QString QgsMssqlTransaction::createSavepoint( const QString &savePointId, QString &error )
+{
+  if ( !mTransactionActive )
+    return QString();
+
+  if ( !executeSql( QStringLiteral( "SAVE TRAN %1" ).arg( QgsExpression::quotedColumnRef( savePointId ) ), error ) )
+  {
+    QgsMessageLog::logMessage( tr( "Could not create savepoint (%1)" ).arg( error ) );
+    return QString();
+  }
+
+  mSavepoints.push( savePointId );
+  mLastSavePointIsDirty = false;
+  return savePointId;
+}
+
+QSqlQuery QgsMssqlTransaction::createQuery()
+{
+  if ( !mDataBase.isOpen() )
+  {
+    if ( !QgsMssqlConnection::openDatabase( mDataBase ) )
+    {
+      QgsDataSourceUri uri = mConnString;
+      mDataBase = QgsMssqlConnection::getDatabaseConnection( uri, mConnString );
+    }
+
+    QgsMssqlConnection::openDatabase( mDataBase );
+    if ( !mDataBase.isOpen() )
+      return QSqlQuery();
+  }
+
+  return QSqlQuery( mDataBase );
+}
+
+bool QgsMssqlTransaction::transactionCommand( const QString command, QString &error )
+{
+  if ( mDataBase.isValid() )
+  {
+    if ( !mDataBase.isOpen() )
+      mDataBase.open();
+
+    if ( mDataBase.isOpen() )
+    {
+      if ( executeSql( command, error ) )
+        return true;
+    }
+  }
+
+  QString msg = tr( "Unable to %1 transaction (MSSQL)" ).arg( command );
+  if ( error.isEmpty() )
+    error = msg;
+  else
+    error = QStringLiteral( "%1\n%2" ).arg( error, msg );
+
+  return false;
+}
+
+bool QgsMssqlTransaction::beginTransaction( QString &error, int )
+{
+  if ( mDataBase.isValid() )
+  {
+
+    if ( !mDataBase.isOpen() )
+      mDataBase.open();
+
+    if ( mDataBase.isOpen() )
+    {
+
+      if ( !mDataBase.driver()->hasFeature( QSqlDriver::Transactions ) )
+        return false;
+
+      if ( mDataBase.transaction() )
+      {
+        return true;
+      }
+    }
+  }
+  error = mDataBase.lastError().text();
+  return false;
+}
+
+bool QgsMssqlTransaction::commitTransaction( QString &error )
+{
+  if ( mDataBase.isValid() && mDataBase.isOpen() )
+  {
+    mDataBase.commit();
+    return true;
+  }
+
+  return false;
+}
+
+bool QgsMssqlTransaction::rollbackTransaction( QString &error )
+{
+  if ( mDataBase.isValid() && mDataBase.isOpen() )
+  {
+    mDataBase.rollback();
+    return true;
+  }
+
+  return false;
+
+}
+
+//****************************** thread safe connection wrapper
+
+void QgsMssqlConnectionWrapper::createQuery( const QString &uid )
+{
+  QMutexLocker locker( &mMutex );
+  mQueries[uid] = QSharedPointer<QSqlQuery>( new QSqlQuery( mDatabaseConnection ) );
+}
+
+void QgsMssqlConnectionWrapper::removeQuery( const QString &uid )
+{
+  QMutexLocker locker( &mMutex );
+  if ( mQueries.contains( uid ) )
+    mQueries.remove( uid );
+}
+
+void QgsMssqlConnectionWrapper::executeQuery( const QString &uid, const QString &query )
+{
+  QMutexLocker locker( &mMutex );
+  if ( mQueries.contains( uid ) )
+    mQueriesSuccesful[uid] = mQueries.value( uid )->exec( query );
+}
+
+QVariant QgsMssqlConnectionWrapper::queryResultValue( const QString &uid, int index ) const
+{
+  QMutexLocker locker( &mMutex );
+  if ( !mQueries.contains( uid ) )
+    return QVariant();
+
+  return mQueries.value( uid )->value( index );
+}
+
+bool QgsMssqlConnectionWrapper::isQuerySuccessful( const QString &uid ) const
+{
+  QMutexLocker locker( &mMutex );
+  return mQueriesSuccesful.value( uid, false );
+}
+
+bool QgsMssqlConnectionWrapper::isQueryActive( const QString &uid ) const
+{
+  QMutexLocker locker( &mMutex );
+  if ( !mQueries.contains( uid ) )
+    return mQueries.value( uid )->isActive();
+  return false;
+}
+
+void QgsMssqlConnectionWrapper::createConnection()
+{
+  QMutexLocker locker( &mMutex );
+  mDatabaseConnection = QgsMssqlConnection::getDatabaseConnection( mUri, mUri.connectionInfo() );
+}
+
+void QgsMssqlConnectionWrapper::beginTransaction()
+{
+  QMutexLocker locker( &mMutex );
+  mQueries.clear();
+  mIsTransactionActive = mDatabaseConnection.transaction();
+}
+
+void QgsMssqlConnectionWrapper::commitTransaction()
+{
+  QMutexLocker locker( &mMutex );
+  mQueries.clear();
+  mDatabaseConnection.commit();
+  mIsTransactionActive = false;
+}
+
+void QgsMssqlConnectionWrapper::rollbackTransaction()
+{
+  QMutexLocker locker( &mMutex );
+  mQueries.clear();
+  mDatabaseConnection.rollback();
+  mIsTransactionActive = false;
+}
+
+//******************************
+
+QgsMssqlThreadSafeConnection::QgsMssqlThreadSafeConnection( const QgsDataSourceUri &uri ):
+  mConnection( new QgsMssqlConnectionWrapper( uri ) )
+{
+  mTransactionThread = new QThread;
+  mConnection->moveToThread( mTransactionThread );
+  connect( mTransactionThread, &QThread::started, mConnection, &QgsMssqlConnectionWrapper::createConnection );
+  connect( mTransactionThread, &QThread::finished, mConnection, &QgsMssqlConnectionWrapper::deleteLater );
+  mTransactionThread->start();
+}
+
+QgsMssqlThreadSafeConnection::~QgsMssqlThreadSafeConnection()
+{
+  mTransactionThread->quit();
+  mTransactionThread->wait();
+}
+
+void QgsMssqlThreadSafeConnection::beginTransaction()
+{
+  if ( mConnection )
+    QMetaObject::invokeMethod( mConnection, "beginTransaction", Qt::BlockingQueuedConnection );
+}
+
+void QgsMssqlThreadSafeConnection::commitTransaction()
+{
+  if ( mConnection )
+    QMetaObject::invokeMethod( mConnection, "commitTransaction", Qt::BlockingQueuedConnection );
+}
+
+void QgsMssqlThreadSafeConnection::rollbackTransaction()
+{
+  if ( mConnection )
+    QMetaObject::invokeMethod( mConnection, "rollbackTransaction", Qt::BlockingQueuedConnection );
+}
+
+QgsMssqlThreadSafeConnection::Query QgsMssqlThreadSafeConnection::createQuery()
+{
+  return QgsMssqlThreadSafeConnection::Query( this );
+}
+
+
+QString QgsMssqlThreadSafeConnection::createQueryPrivate()
+{
+  if ( !mConnection )
+    return QString();
+  QString uid = QUuid::createUuid().toString();
+  QMetaObject::invokeMethod( mConnection, "createQuery", Qt::BlockingQueuedConnection, Q_ARG( QString, uid ) );
+  return uid;
+}
+
+void QgsMssqlThreadSafeConnection::removeQueryPrivate( const QString &queryId )
+{
+  if ( mConnection )
+    QMetaObject::invokeMethod( mConnection, "removeQuery", Qt::BlockingQueuedConnection, Q_ARG( QString, queryId ) );
+}
+
+bool QgsMssqlThreadSafeConnection::isQuerySucessfull( const QString &queryId )
+{
+  if ( mConnection )
+    return mConnection->isQuerySuccessful( queryId );
+  return false;
+}
+
+
+void QgsMssqlThreadSafeConnection::executeQuery( const QString &query, const QString &uid )
+{
+  if ( mConnection )
+    QMetaObject::invokeMethod( mConnection, "executeQuery", Qt::BlockingQueuedConnection, Q_ARG( QString, query ), Q_ARG( QString, uid ) );
+}
+
+bool QgsMssqlThreadSafeConnection::queryIsActive( const QString &queryId ) const
+{
+  if ( mConnection )
+    mConnection->isQueryActive( queryId );
+  return false;
+}
+
+QgsMssqlThreadSafeConnection::Query::Query( QgsMssqlThreadSafeConnection *connection ):
+  mConnection( connection )
+{
+  if ( !mConnection.isNull() )
+    mUid = mConnection->createQueryPrivate();
+}
+
+QgsMssqlThreadSafeConnection::Query::~Query()
+{
+  if ( !mConnection.isNull() )
+    mConnection->removeQueryPrivate( mUid );
+}
+
+bool QgsMssqlThreadSafeConnection::Query::exec( const QString query )
+{
+  if ( mConnection.isNull() )
+    return false;
+  mConnection->executeQuery( mUid, query );
+  return mConnection->isQuerySucessfull( mUid );
+}
+
+bool QgsMssqlThreadSafeConnection::Query::isActive()
+{
+  if ( mConnection.isNull() )
+    return false;
+  mConnection->queryIsActive( mUid );
+}
---
+++ tests/src/providers/testqgsmssqlprovider.cpp
@@ -0,0 +1,78 @@
+/***************************************************************************
+  qgis_mssqlprovidertest.cpp - %{Cpp:License:ClassName}
+
+ ---------------------
+ begin                : 16.3.2021
+ copyright            : (C) 2021 by Vincent Cloarec
+ email                : vcloarec at gmail dot com
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+
+#include "qgstest.h"
+
+
+//qgis includes...
+#include <qgis.h>
+#include <qgsapplication.h>
+#include <qgsmssqltransaction.h>
+#include <qgsmssqlconnection.h>
+
+/**
+ * \ingroup UnitTests
+ * This is a unit test for the gdal provider
+ */
+class TestQgsMssqlProvider : public QObject
+{
+    Q_OBJECT
+
+  private slots:
+    void initTestCase();// will be called before the first testfunction is executed.
+    void cleanupTestCase();// will be called after the last testfunction was executed.
+    void init() {}// will be called before each testfunction is executed.
+    void cleanup() {}// will be called after every testfunction.
+
+    void threadSafeConnection();
+
+  private:
+
+};
+
+//runs before all tests
+void TestQgsMssqlProvider::initTestCase()
+{
+  // init QGIS's paths - true means that all path will be inited from prefix
+  QgsApplication::init();
+  QgsApplication::initQgis();
+}
+
+
+//runs after all tests
+void TestQgsMssqlProvider::cleanupTestCase()
+{
+  QgsApplication::exitQgis();
+}
+
+void TestQgsMssqlProvider::threadSafeConnection()
+{
+  QgsDataSourceUri uri;
+  uri.setConnection( "localhost", "", "qgis", "sa", "<YourStrong!Passw0rd>" );
+  QSqlDatabase dataBase = QgsMssqlConnection::getDatabaseConnection( uri, "simpleConnection" );
+
+  QVERIFY( dataBase.isValid() );
+  QVERIFY( dataBase.open() );
+
+  QgsMssqlThreadSafeConnection threadSafeConnection( uri );
+
+  QgsMssqlThreadSafeConnection::Query query = threadSafeConnection.createQuery();
+
+}
+
+QGSTEST_MAIN( TestQgsMssqlProvider )
+#include "testqgsmssqlprovider.moc"
